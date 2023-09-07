4bcdf7c5dbabb09bbbea8742ee2a28b4e5562952
joshuandungu <joshuandungu2001@gmail.com>
2023-09-07 05:22:26 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/*
+ * File: 101-quote.c
+ * Auth: joshua ndungu
+ *  a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ */
+
+#include <unistd.h>
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
