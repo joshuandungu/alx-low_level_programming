298b0e1c43bc0580f3732f56535a9490c1e3cb7c
joshuandungu <joshuandungu2001@gmail.com>
2023-09-07 04:53:49 -0700

    C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/*
+ * File: 5-printf.c
+ * Auth: joshua ndungu
+ */
+/* C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line. */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
