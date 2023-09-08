e7e64b4109a0f259edb7e7370c03c859d96959a7
joshuandungu <joshuandungu2001@gmail.com>
2023-09-08 03:30:58 -0700

    A progrm that print all single numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 /*
  * File: 5-print_numbers.c
- * Auth: Joshua ndungu
+ * Auth: joshu ndungu
  */
 
 #include <stdio.h>
@@ -13,8 +13,10 @@
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		printf("%d", num);
-	print("\n");
+
+	printf("\n");
+
 	return (0);
-}
