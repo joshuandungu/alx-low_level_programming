f280802fb909354033be17d6c60ad5def01e4fa0
joshuandungu <joshuandungu2001@gmail.com>
2023-09-08 02:10:32 -0700

    Program to print numbers either positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,25 @@
-/* file 0-positive_or_negative.c
- * Author:joshua ndungu
- */
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-/* This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number* stored in the variable n is positive or negative.
- */
+#include <time.h>
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n > 0)
+
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else 
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
 		printf("%d is zero\n", n);
+
 	return (0);
 }
-	
