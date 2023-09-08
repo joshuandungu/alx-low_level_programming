adb4f5aed40b45c72f8c519108d77bc8a97bcf73
joshuandungu <joshuandungu2001@gmail.com>
2023-09-08 03:08:01 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,26 @@
-/* 4-print_alphabt.c
- * Auth :Joshua ndungu
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Joshua ndungu
  */
 
 #include <stdio.h>
-/* main - prints letters in lowercase except fo e and q .
- * return (0);
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'e'  && letter != 'q')
-		putchar(letter);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
