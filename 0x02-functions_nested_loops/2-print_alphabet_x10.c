a9b98425d03517e81e30bcc3813194bfe3c5e9d0
joshuandungu <joshuandungu2001@gmail.com>
2023-09-12 01:40:52 -0700

    Nested while loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/*
+ * file : 2-print_alphabet_x10.c
+ * author : joshua ndungu
+ */
+#include <main.h>
+/** 
+ * main -reads the code
+ *  prints 10 times the alphabet, in lowercase, followed by a new line.
+ * return always 0
+ */
+int main(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for ( letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
