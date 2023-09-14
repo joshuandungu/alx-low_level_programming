43ba6c1442a48b9f5b0422a16e9befa5e8e9c262
joshuandungu <joshuandungu2001@gmail.com>
2023-09-14 12:24:28 -0700

    New changes

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,16 +1,23 @@
+/*
+ * file :main.c
+ * auth: joshua ndungu
+ */
+
+
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive, negative, or zero
+ * Return: 0
+ */
 
 int main(void)
 {
         int i;
 
-        i = 0;
+        i = 0; /* Change this line to test with 0 */
         positive_or_negative(i);
 
         return (0);
 }
+
