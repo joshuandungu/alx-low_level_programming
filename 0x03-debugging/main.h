5dfc1305d588bd861168fad3f3be01675fc9b8d8
joshuandungu <joshuandungu2001@gmail.com>
2023-09-15 11:54:18 -0700

    new changes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,14 @@
+/* file :main.h
+ * auth : joshua ndungu
+ */
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * positive_or_negative - print a positive or negative number
+ * largest_number - prints the largest number
+ * convert_day -convert day into months and years
+ * print_remainin_days - prints the remaining days
+ */
 #include <stdio.h>
 
 void positive_or_negative(int i);
