ea9b222990b108e8a5104ab23dbae8e614d6e711
joshuandungu <joshuandungu2001@gmail.com>
2023-09-18 05:45:59 -0700

    Resolve conflicts

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
+	char *str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	 print_rev(str);
+	 return (0);
 }
