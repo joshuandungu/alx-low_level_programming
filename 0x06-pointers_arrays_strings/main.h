22ff3432d0e879a02cc15d877285249f1b25648f
joshuandungu <joshuandungu2001@gmail.com>
2023-09-20 01:31:15 -0700

    function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * File: main.h
+ * Auth: joshua ndungu
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+char *_strcat(char *dest, char *src);
+
+#endif
+
