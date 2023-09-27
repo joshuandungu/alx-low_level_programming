be3fb9daa0efc0e5005c2843b3ad4ed237115d47
joshuandungu <joshuandungu2001@gmail.com>
2023-09-27 11:56:13 +0300

    Recursion functions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - Prints a string, followed by a new line.
@@ -9,7 +10,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		putchar(*s);
-		puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 
 	else
