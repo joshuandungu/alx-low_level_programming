9f59d386e297fb198c134a5fd69853e9b0359b81
joshuandungu <joshuandungu2001@gmail.com>
2023-10-11 12:27:22 -0700

    File containing all function prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,18 +4,10 @@
 
 #include <stddef.h>
 #include <stdlib.h>
-/**
- * function_pointers.h - contain all function prototypes
- * void print_name(chat *name, void (*f) char *))  function that prints a name
- * int _putchar(char c - executes a function given array as an elemt parameter
- * void array_iterator(int *array, size_t size, void (*action)(int))
- * int int_index(int *array, int size, int (*cmp)(int)) -do simple operation
- */
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-
-	#endif
+#endif
 
